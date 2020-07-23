2da1a588630942d0a512a153ee19d6d0ca1d7a06
dhk5844 <dhk5844@ajou.ac.kr>
2020-07-23 15:21:05 +0900

    Add comments

--- src/main/java/repository/MockRepository.java
+++ src/main/java/repository/MockRepository.java
@@ -4,7 +4,7 @@ import domain.Champion;
 
 import java.util.List;
 
-public interface MockRepository {
+public interface MockRepository { // 저장소 개념
     List<Champion> findAll();
 
     Champion findByName(String name);
