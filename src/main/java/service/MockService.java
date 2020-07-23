bfb92d9135547aeebcded0982c7ff0be8be28331
dhk5844 <dhk5844@ajou.ac.kr>
2020-07-23 15:21:13 +0900

    Add comments

--- src/main/java/service/MockService.java
+++ src/main/java/service/MockService.java
@@ -11,23 +11,23 @@ public class MockService {
     public MockService(MockRepository mockRepository) {
         this.mockRepository = mockRepository;
     }
-
+    //챔피언 모두 찾기
     public List<Champion> findAllChampions() {
         return mockRepository.findAll();
     }
-
+    //챔피언 이름으로 찾기
     public Champion findByName(String name) {
         Champion champion = mockRepository.findByName(name);
         return champion;
     }
-
+    // 포지션 변경
     public Champion updatePositionByName(String name, String position) {
         Champion champion = findByName(name);
         champion.setName(position);
 
         return champion;
     }
-
+    //챔피언 추가
     public void addChampion(Champion champion){
         Champion addChampion = new Champion(champion.getName(), champion.getPosition(), champion.getHasSkinCount());
         mockRepository.addChampion(addChampion);
